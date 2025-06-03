8db916b8b12268325eb8fbd9a52892301797f805
Meenakshi Kumari <meenakshigauri17@gmail.com>
2025-06-03 15:52:14 +0530

    Add files via upload

---
+++ Dijkstra_Algo_TreeSet.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Pair{
+    int dist;
+    int node;
+    public Pair(int d, int n){
+        this.dist = d;
+        this.node = n;
+    }
+}
+
+class Solution{
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S){
+        TreeSet<Pair> set = new TreeSet<>((a,b) -> (a.dist ==  b.dist) ? (a.node - b.node) : (a.dist-b.dist));
+        int[] dist = new int[V];
+        Arrays.fill(dist,(int)1e9);
+        dist[S] = 0;
+
+        set.add(new Pair(0,S));
+
+        while(!set.isEmpty()){
+            Pair curr = set.pollFirst();
+
+            for(Pair p : adj.get(curr.node)){
+                int wt = p.dist;
+                int n = p.node;
+                int newDist = curr.dist + wt;
+                if(newDist < dist[n]){
+                    set.add(new Pair(newDist,n));
+                }
+                if(dist[n] < 1e9){
+                    set.remove(new Pair(dist[n],n));
+                }
+
+                dist[n] = newDist;
+            }
+        }
+
+
+        return dist;
+
+
+    }
+}
